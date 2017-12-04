9fdf852ee601ee9fdda33200fe8326ff15353393
Anne-Claire Bourland <ab14188@my.bristol.ac.uk>
2017-12-04 12:58:34 +0000

    cmds to run in readme adn submit_job bash skeleton

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 traffic-sign-recognition
+
+run live on bc4 :
+	* srun -N 1 -p gpu --pty bash  
+or  * srun -N 1 -p gpu -- gres=gpu:1 --mem=4G --pty bash
+
+
+submitting a job to bc4 :
+	* sbatch submit_job.sh
+	* to look at the results open the slurm_### file 
---
+++ submit_job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -login
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --partition gpu
+#SBATCH --job-name=gpujob
+#SBATCH -A comsm0018
+#SBATCH -t 0-02:00
+#SBATCH --mem=4G
+
+module load libs/tensorflow/1.2
+rm -rf logs
+mkdir logs 
+#python file.py file to run	
